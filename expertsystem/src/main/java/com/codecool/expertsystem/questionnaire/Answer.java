3e030ab63211c49f33f90457f767138846f6f1d7
Maziarella <kiebala.magdalena@gmail.com>
2018-07-18 22:58:29 +0200

    Add package iterators with implement classes FactIterator and QuestionIterator

--- expertsystem/src/main/java/com/codecool/expertsystem/questionnaire/Answer.java
+++ expertsystem/src/main/java/com/codecool/expertsystem/questionnaire/Answer.java
@@ -1,4 +1,43 @@
 package com.codecool.expertsystem.questionnaire;
 
+import com.codecool.expertsystem.questionnaire.value.Value;
+import com.codecool.expertsystem.view.Display;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Answer {
+
+
+    private Display display = new Display();
+    private List<Value> valuesList = new ArrayList<>();
+
+
+    public boolean evaluateAnswerByInput(String input) {
+
+        List<String> values = new ArrayList<>();
+        values = Arrays.asList(input.split(","));
+
+        for (Value value : valuesList) {
+
+            for (String param : values) {
+
+                if (value.getInputPattern().contains(param)) {
+                    return value.getSelectionType();
+                }
+            }
+        }
+        return false;
+    }
+
+
+
+    public List<Value> getValuesList() {
+        return valuesList;
+    }
+
+    public void addValue(Value value) {
+        valuesList.add(value);
+    }
 }
