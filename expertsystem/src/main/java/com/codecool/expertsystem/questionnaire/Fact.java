ed7f6c4e6c8acea82ab3ea459b19e956cee69f20
Maziarella <kiebala.magdalena@gmail.com>
2018-07-18 23:03:27 +0200

    In package questionnaire implement classes Answer, fact and Question

--- expertsystem/src/main/java/com/codecool/expertsystem/questionnaire/Fact.java
+++ expertsystem/src/main/java/com/codecool/expertsystem/questionnaire/Fact.java
@@ -1,4 +1,46 @@
 package com.codecool.expertsystem.questionnaire;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
+
 public class Fact {
+
+    private Map<String, Boolean> valueMap;
+    private String description;
+    private String id;
+
+    public Fact(String id, String description) {
+
+        initializeMap();
+        this.id = id;
+        this.description = description;
+
+    }
+
+    private void initializeMap() {
+
+        this.valueMap = new HashMap<>();
+
+    }
+
+    public void setFactValueById(String id, boolean value) {
+
+        this.valueMap.put(id, value);
+
+    }
+
+    public String getDescription() {
+
+        return this.description;
+
+    }
+
+    public boolean getValueById(String id) {
+
+        return this.valueMap.get(id);
+
+    }
+
 }
--- expertsystem/src/main/java/com/codecool/expertsystem/questionnaire/Question.java
+++ expertsystem/src/main/java/com/codecool/expertsystem/questionnaire/Question.java
@@ -1,4 +1,34 @@
 package com.codecool.expertsystem.questionnaire;
 
 public class Question {
+
+    private final String ID;
+    private final String QUESTION;
+    private final Answer ANSWER;
+
+    public Question(String id, String question, Answer answer) {
+
+        this.ID = id;
+        this.ANSWER = answer;
+        this.QUESTION = question;
+
+    }
+
+
+    public String getId() {
+        return this.ID;
+    }
+
+    public String getQuestion() {
+
+        return this.QUESTION;
+    }
+
+    public Answer getAnswer() {
+        return this.ANSWER;
+    }
+
+    public boolean getEvaluatedAnswer(String input) {
+        return ANSWER.evaluateAnswerByInput(input);
+    }
 }
